17a18420424d48236921c2f63979b05c39a41323
kryvetskyi <kryvetskyi@gmail.com>
2023-02-23 14:27:33 +0200

    Added ansible docker terraform directories

---
+++ terraform/jenkins/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+variable "name" {
+  description = "Name the instance on deploy"
+}
+
+resource "aws_instance" "devops_jenkins" {
+  ami = "ami-01a2825a801771f57"
+  instance_type = "t2.micro"
+  key_name = "jenkins"
+
+  tags = {
+    Name = "${var.name}"
+  }
+}
